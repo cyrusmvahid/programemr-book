2f95c1fc564119d54601beba5d54dee245926306
Moazami Vahid <cyrusmv@9801a7adee53.ant.amazon.com>
2016-08-25 15:20:11 +0200

    aaded Author

--- PythonCode/log_analysis.py
+++ PythonCode/log_analysis.py
@@ -1,3 +1,12 @@
+"""
+Authon: Cyrus M Vahid
+This code looks the date entry in a log file supplied in /Data/sample-log.log and counts the events
+that occured in a specific timestamp, aggregated every second.
+The mapper splits the lines and reducer sums up the occurences per second.
+
+To run the app:
+python log_analysis.py Data/sample-log.log
+"""
 from mrjob.job import MRJob
 
 class LogProcessor(MRJob):
