6dc8bf9cffdc110814d43724399ecf25cad0a3de
cyrusmvahid@gmail.com <cyrusmvahid@gmail.com>
2016-08-26 13:48:04 +0200

    nasa-log-added

---
+++ PythonCode/nasa-log-processor.py
@@ -0,0 +1,36 @@
+from mrjob.job import MRJob
+import re
+
+class LogAnalyzer(MRJob):
+
+    def mapper(self, _, line):
+        pattern = '^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(.+?)" (\d{3}) (\S+)'
+        file = '/Users/cyrusmv/PycharmProjects/programemr-book/PythonCode/Data/NASA_access_log_Jul95.log'
+        p = re.compile(pattern)
+        m = p.match(line)
+        if m:
+            i = int(m.group(6))
+            if i in range(100, 200):
+                yield "100-199: Informational", 1
+            elif i in range(200, 300):
+                yield "200-299: All OK", 1
+            elif i in range(300,400):
+                yield "300 to 399: Redirection", 1
+            elif i in range(400, 500):
+                yield "400 to 499: Client Error", 1
+            elif i in range(500, 600):
+                yield "500 to 599: Server Error", 1
+        else:
+            yield 'format mismatch', 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    LogAnalyzer.run()
+
+
+
+
+
+
